8bedac950f8be776daa9a642b53783156dddca9c
James <james.lapenn@gmail.com>
2011-06-16 23:41:42 -0400

    working random to limited degree

--- src/main/java/me/passivepicasso/infinitedungeon/InfiniteDungeon.java
+++ src/main/java/me/passivepicasso/infinitedungeon/InfiniteDungeon.java
@@ -9,7 +9,7 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -31,10 +31,10 @@ public class InfiniteDungeon extends JavaPlugin {
         infiniteDungeon = getServer().createWorld("InfiniteDungeon", World.Environment.NORMAL, new ChunkGenerator() {
             @Override
             public byte[] generate(World world, Random random, int x, int z) {
-                if (x == 0 && z == 0) {
-                    return emptyChunk;
-                }
-                return chunkGenArray;
+//                if (x == 0 && z == 0) {
+//                    return emptyChunk;
+//                }
+                return chunkGenArray.clone();
             }
 
             @Override
@@ -44,18 +44,13 @@ public class InfiniteDungeon extends JavaPlugin {
 
             @Override
             public Location getFixedSpawnLocation(World world, Random random) {
-                return new Location(world, 5, 5, 5);
+                return new Location(world, 5, 127, 5);
             }
 
-            private ArrayList<BlockPopulator> populators = null;
 
             @Override
             public List<BlockPopulator> getDefaultPopulators(World world) {
-                if(populators == null){
-                    populators = new ArrayList<BlockPopulator>();
-                    populators.add(new RoomPopulator());
-                }
-                return populators;
+                return Arrays.asList((BlockPopulator)new RoomPopulator());
             }
         });
 
@@ -79,12 +74,12 @@ public class InfiniteDungeon extends JavaPlugin {
         for (int cx = 0; cx < 16; cx++) {
             for (int cz = 0; cz < 16; cz++) {
                 for (int y = 0; y < 128; y++) {
-                    if (y == 0) {
-                        emptyChunk[(cx * 16 + cz) * 128 + y] = (byte) Material.BEDROCK.getId();
-                    } else {
-                        emptyChunk[(cx * 16 + cz) * 128 + y] = (byte) Material.AIR.getId();
-                    }
-                    if (y == 0 || y == 127) {
+//                    if (y == 0) {
+//                        emptyChunk[(cx * 16 + cz) * 128 + y] = (byte) Material.BEDROCK.getId();
+//                    } else {
+//                        emptyChunk[(cx * 16 + cz) * 128 + y] = (byte) Material.AIR.getId();
+//                    }
+                    if (y == 0 || y == 0) {
                         chunkGenArray[(cx * 16 + cz) * 128 + y] = (byte) Material.BEDROCK.getId();
                     } else {
                         chunkGenArray[(cx * 16 + cz) * 128 + y] = (byte) Material.STONE.getId();
--- src/main/java/me/passivepicasso/infinitedungeon/RoomPopulator.java
+++ src/main/java/me/passivepicasso/infinitedungeon/RoomPopulator.java
@@ -1,12 +1,15 @@
 package me.passivepicasso.infinitedungeon;
 
+import me.passivepicasso.util.BlockMatrixNode;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.generator.BlockPopulator;
-import org.bukkit.util.BlockVector;
-import org.bukkit.util.Vector;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -18,28 +21,142 @@ import java.util.Random;
  */
 public class RoomPopulator extends BlockPopulator {
 
+    private static final int COORIDORE_CHANCE = 50;
+    private static final int ROOM_CHANCE = 30;
+    private static final int DOUBLEHEIGHT_CHANCE = 10;
 
     @Override
     public void populate(World world, Random random, Chunk source) {
-        random.setSeed(random.nextLong());
-        int center_x = random.nextInt(16) + (16 * source.getX());
-        int center_z = random.nextInt(16) + (16 * source.getZ());
-        int level = 1;
-        int y_base = 127 - ((4 * level) + 1);
+        if (random.nextInt(100) < ROOM_CHANCE) {
+            int center_x = 0;
+            if (source.getX() >= 0) {
+                center_x = 7 + (source.getX() * 16) + (4 - random.nextInt(8));
+            } else {
+                center_x = (source.getX() * 16) - 7 + (4 - random.nextInt(8));
+            }
 
-        Vector startingPoint = new BlockVector(center_x, y_base, center_z);
+            int center_z = 0;
+            if (source.getZ() >= 0) {
+                center_z = 7 + (source.getZ() * 16) + (4 - random.nextInt(8));
+            } else {
+                center_z = (source.getZ() * 16) - 7 + (4 - random.nextInt(8));
+            }
+            int level = 1;
+            int y_base = 127 - ((4 * level) + 1);
+            int y_boost = 0;
+            if (random.nextInt(100) < DOUBLEHEIGHT_CHANCE) {
+                y_boost = 4;
+                y_base -= 4;
+            }
 
-
-        int halfWidth = random.nextInt(3) + 3;
-        int halfLength = random.nextInt(3) + 3;
-
-        for (int x = -halfWidth; x <= halfWidth; x++) {
-            for (int z = -halfLength; z <= halfLength; z++) {
-                for (int y = 1; y < 5; y++) {
-                    Vector target = startingPoint.clone().add(new Vector(x, y, z));
-                    world.getBlockAt(target.toLocation(world)).setType(Material.AIR);
+            int halfWidth = random.nextInt(6) + 3;
+            int halfLength = random.nextInt(6) + 3;
+            HashSet<Block> room = new HashSet<Block>();
+            for (int x = (center_x - halfWidth); x <= (center_x + halfWidth); x++) {
+                for (int z = (center_z - halfLength); z <= (center_z + halfLength); z++) {
+                    for (int y = y_base + 1; y <= y_base + 4 + y_boost; y++) {
+                        Block block = world.getBlockAt(x, y, z);
+                        block.setType(Material.AIR);
+                        room.add(block);
+                    }
                 }
             }
+            HashMap<Block, BlockMatrixNode> matrix = new HashMap<Block, BlockMatrixNode>();
+            for (Block block : room) {
+                new BlockMatrixNode(block, matrix);
+            }
+            BlockMatrixNode rootnode = matrix.values().iterator().next();
+
+            randomCooridoor(rootnode, random, random.nextInt(4), true);
+        }
+    }
+
+
+    /**
+     * Always pass true for firstIteration except from within firstIteration.
+     *
+     * @param node
+     * @param random
+     * @param direction
+     * @param firstIteration
+     */
+    private void randomCooridoor(BlockMatrixNode node, Random random, int direction, boolean firstIteration) {
+        node.setFilter(new HashSet<Material>(EnumSet.of(Material.AIR)));
+        int dir = random.nextInt(4);
+        switch (dir) {
+            case 0:
+                while (node.hasFilteredNorth()) {
+                    node = node.getNorth();
+                }
+                node.setFilter(new HashSet<Material>(EnumSet.of(Material.STONE)));
+                int maxDistanceN = random.nextInt(12) + 4;
+                while (node.hasFilteredNorth()) {
+                    node.getBlock().setType(Material.AIR);
+                    node = node.getNorth();
+                    if (maxDistanceN == 0) {
+                        break;
+                    }
+                    maxDistanceN--;
+                }
+                if (firstIteration) {
+                    randomCooridoor(node, random, random.nextInt(4), false);
+                }
+                break;
+            case 1:
+                while (node.hasFilteredEast()) {
+                    node = node.getEast();
+                }
+                node.setFilter(new HashSet<Material>(EnumSet.of(Material.STONE)));
+                int maxDistanceE = random.nextInt(12) + 4;
+                while (node.hasFilteredEast()) {
+                    node.getBlock().setType(Material.AIR);
+                    node = node.getEast();
+                    if (maxDistanceE == 0) {
+                        break;
+                    }
+                    maxDistanceE--;
+                }
+                if (firstIteration) {
+                    randomCooridoor(node, random, random.nextInt(4), false);
+                }
+                break;
+            case 2:
+                while (node.hasFilteredSouth()) {
+                    node = node.getSouth();
+                }
+                node.setFilter(new HashSet<Material>(EnumSet.of(Material.STONE)));
+                int maxDistanceS = random.nextInt(12) + 4;
+                while (node.hasFilteredSouth()) {
+                    node.getBlock().setType(Material.AIR);
+                    node = node.getSouth();
+                    if (maxDistanceS == 0) {
+                        break;
+                    }
+                    maxDistanceS--;
+                }
+                if (firstIteration) {
+                    randomCooridoor(node, random, random.nextInt(4), false);
+                }
+                break;
+            case 3:
+                while (node.hasFilteredWest()) {
+                    node = node.getWest();
+                }
+                node.setFilter(new HashSet<Material>(EnumSet.of(Material.STONE)));
+                int maxDistanceW = random.nextInt(12) + 4;
+                while (node.hasFilteredWest()) {
+                    node.getBlock().setType(Material.AIR);
+                    node = node.getWest();
+                    if (maxDistanceW == 0) {
+                        break;
+                    }
+                    maxDistanceW--;
+                }
+                if (firstIteration) {
+                    randomCooridoor(node, random, random.nextInt(4), false);
+                }
+                break;
+
         }
     }
 }
